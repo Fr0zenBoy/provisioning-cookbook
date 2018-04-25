fbc4391899a28a33d054c2f3e7740b8def32e383
thiagodocket <thiago.silva@docket.com.br>
2018-04-25 11:04:40 -0300

    first commit

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name 'provisioning'
+maintainer 'Thiagodocket'
+maintainer_email 'thiago.silva@docket.com.br'
+license 'all_rights'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'delivery-truck'
