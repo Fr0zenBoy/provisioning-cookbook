468d42d30aa59e2805a7cdd5e79cf1cd6c4ac7e7
thiagodocket <thiago.silva@docket.com.br>
2018-04-26 18:34:32 -0300

    fix

--- recipes/prebuild.rb
+++ recipes/prebuild.rb
@@ -2,39 +2,43 @@ include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
 
-git deploy '/home/ubuntu/projetos/admin' do
-  repo 'https://github.com/cartorioscomvc/admin.git'
-  revision master
-  user 'cartorioscomvc'
-  ssh_wrapper 'id_rsa.pub'
-  action :sync
+    git deploy '/home/ubuntu/projetos/admin' do
+      repo 'https://github.com/cartorioscomvc/admin.git'
+      revision master
+      user 'cartorioscomvc'
+      ssh_wrapper 'id_rsa.pub'
+      action :sync
+    end
+
+    git '/home/ubuntu/projetos/core' do
+      repo 'https://github.com/cartorioscomvc/core.git'
+      revision master
+      user 'cartorioscomvc'
+      ssh_wrapper '/tmp/private_code/wrap-ssh4git.sh'
+      action :sync
+    end
+
+    execute 'artefact core' do
+      command 'mvn clean install -Dmaven.test.skip=true -f /home/ubuntu/projetos/core/pom.xml'
+      action :run
+    end
+
+    execute 'artefact admin' do
+      command 'mvn clean install -Dmaven.test.skip=true -f /home/ubuntu/projetos/admin/pom.xml'
+      action :run
+    end
+
+
+    execute 'rename artefact ROOT.war' do
+      command 'sh /home/ubuntu/scripts/mv-root.sh'
+      action :run
+    end
+
+    execute 'upload to s3 aws' do
+      command 'sh /home/ubuntu/scripts/s3-upload.sh'
+      action :run
+    end
+
 end
 
-git '/home/ubuntu/projetos/core' do
-  repo 'https://github.com/cartorioscomvc/core.git'
-  revision master
-  user 'cartorioscomvc'
-  ssh_wrapper '/tmp/private_code/wrap-ssh4git.sh'
-  action :sync
-end
 
-execute 'artefact core' do
-  command 'mvn clean install -Dmaven.test.skip=true -f /home/ubuntu/projetos/core/pom.xml'
-  action :run
-end
-
-execute 'artefact admin' do
-  command 'mvn clean install -Dmaven.test.skip=true -f /home/ubuntu/projetos/admin/pom.xml'
-  action :run
-end
-
-
-execute 'rename artefact ROOT.war' do
-  command 'sh /home/ubuntu/scripts/mv-root.sh'
-  action :run
-end
-
-execute 'upload to s3 aws' do
-  command 'sh /home/ubuntu/scripts/s3-upload.sh'
-  action :run
-end
